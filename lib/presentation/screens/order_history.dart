5378bf6460dbe53f86117804844aa4b9955474c2
Jainaveen-M <jainaveen@giottus.com>
2024-05-31 00:15:07 +0530

    added empty check

--- lib/presentation/screens/order_history.dart
+++ lib/presentation/screens/order_history.dart
@@ -24,63 +24,75 @@ class _OrderHistoryState extends State<OrderHistory> {
         listener: (context, state) {},
         builder: (context, state) {
           if (state is OrderHistoryLoaded) {
-            return ListView.builder(
-                itemCount: state.orderHistory.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 1.0,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                state.orderHistory[index].code,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 16),
-                              ),
-                              Text(
-                                state.orderHistory[index].orderType,
-                                style: TextStyle(
-                                  color: state.orderHistory[index].orderType ==
-                                          "Buy"
-                                      ? Colors.green
-                                      : Colors.red,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Qty: ${state.orderHistory[index].qty}',
-                                style: TextStyle(fontSize: 14),
-                              ),
-                              Text(
-                                'Price: \$${state.orderHistory[index].price}',
-                                style: TextStyle(fontSize: 14),
-                              ),
-                              Text(
-                                'Total: \$${state.orderHistory[index].total}',
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+            return state.orderHistory.isEmpty
+                ? const Center(
+                    child: Text(
+                      "No trades to show",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                     ),
-                  );
-                });
+                  )
+                : ListView.builder(
+                    itemCount: state.orderHistory.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 1.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    state.orderHistory[index].code,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  Text(
+                                    state.orderHistory[index].orderType,
+                                    style: TextStyle(
+                                      color:
+                                          state.orderHistory[index].orderType ==
+                                                  "Buy"
+                                              ? Colors.green
+                                              : Colors.red,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 8),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Qty: ${state.orderHistory[index].qty}',
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                  Text(
+                                    'Price: \$${state.orderHistory[index].price}',
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                  Text(
+                                    'Total: \$${state.orderHistory[index].total}',
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    });
           }
           return const Center(child: CircularProgressIndicator());
         });
